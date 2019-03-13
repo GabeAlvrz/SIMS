f997a244a63fe9e6a7daf5d6c76d776157fe57bf
ZKWong <zhikang.wong@siu.edu>
2019-03-13 16:17:36 -0500

    Update IGradSeniorSurveyRepository.cs

--- SIMS.API/Data/IGradSeniorSurveyRepository.cs
+++ SIMS.API/Data/IGradSeniorSurveyRepository.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SIMS.API.Models;
 namespace SIMS.API.Data
 {
-    public class IGradSeniorSurveyRepository
+    public interface IGradSeniorSurveyRepository
     {
-        
+        void Add<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+
+        Task<GradSeniorSurvey> GetGradSeniorSurvey(int Id);
+        Task<IEnumerable<GradSeniorSurvey>> GetGradSeniorSurveys();
+        Task<bool> SaveAll();
     }
 }
