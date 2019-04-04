a911c83eb22cc8556ad60490709a0363d359e1cd
ZKWong <zhikang.wong@siu.edu>
2019-04-04 15:50:46 -0500

    Update ThesisProject.cs

--- SIMS.API/Models/ThesisProject.cs
+++ SIMS.API/Models/ThesisProject.cs
@@ -1,7 +1,13 @@
+using System;
 namespace SIMS.API.Models
 {
     public class ThesisProject
     {
-        
+        public int Id { get; set;}
+        public string studentName { get; set; }
+        public string docType { get; set; }
+        public string topic { get; set; }
+        public string fileName { get; set; }
+        public string url { get; set; }
     }
-}
+} 
